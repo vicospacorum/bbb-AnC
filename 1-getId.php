f61dbcd80bc65a42f6acd83228cb696527191646
vicospacorum <fdomingos@usp.br>
2021-04-27 09:44:58 -0300

    Getting IDs

---
+++ 1-getId.php
@@ -0,0 +1,60 @@
+<?php
+    $today = date("Y-m-d H:i:s");
+    echo "========== " . $today . "==========\n";
+
+    $url="https://api.mynaparrot.com/bigbluebutton/hellatech/api/getMeetings?checksum=916fdcc09f4bf874086516b49e3a67ccf88731bc";
+    $ch = curl_init();
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+    curl_setopt($ch, CURLOPT_URL, $url);    // get the url contents
+
+    $data = curl_exec($ch); // execute curl request
+    curl_close($ch);
+    
+    $xml = simplexml_load_string($data);
+    echo $url;
+    echo "\n";
+    print_r($data);
+
+    foreach($xml->meetings->meeting as $sala)
+    {
+        //$xml->meetings->meeting->meetingID;
+        $Id = (string) $sala->meetingID;
+        $IdInterno = (string) $sala->internalMeetingID;
+        $DataHora = (int) $sala->startTime;
+
+        echo $DataHora;
+        echo "\n";
+        $DataHora /= 1000;
+        $DataHora = (int) $DataHora;
+        echo $DataHora;
+        echo "\n";
+
+        
+        if (!empty($IdInterno))
+        {
+            require_once 'connectDB.php';
+
+            $sql = "INSERT IGNORE INTO novas (IdInterno, Id, Inicio) VALUES ('" . $IdInterno . "', '" . $Id . "', " . $DataHora . ");";
+            echo $sql;
+            echo "\n";
+            
+            try
+            {
+                $resultado = $conecta->query($sql);
+                
+                // aaaa-mm-dd hh:mm:ss (the MySQL DATETIME format)
+                $today = date("Y-m-d H:i:s");
+                echo $today . ": Operação realizada com sucesso!\n";
+            }
+            catch(PDOException $e)
+            {
+                echo 'ERRO!';
+                echo $e;
+            }
+        }
+        else
+        {
+            echo "Nenhuma Sessão em Andamento\n";
+        }
+    }
+?>
--- README.md
+++ README.md
@@ -1,1 +1,15 @@
-# automatic-control
+# Big Blue Button Automation and Control
+The aim of this project is to create a series of PHP scripts that will run on Cron and automate the proccess of controlling BigBlueButton's sessions.
+
+## Technologies
+- PHP 7
+- MySQL
+
+---
+
+## Scripts
+
+#### 1-getId.php
+Needs to be executed during sessions. Adds Id, InternalId and Start Time (in UNIXTIME) to table `new`.
+
+###### Runs 4x per hour
