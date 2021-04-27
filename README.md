068d3beb77e0bcf917966639a7e3b85aa03492b4
vicospacorum <fdomingos@usp.br>
2021-04-27 13:40:33 -0300

    downloads the requested Reports

--- README.md
+++ README.md
@@ -10,18 +10,21 @@ The aim of this project is to create a series of PHP scripts that will run on Cr
 
 ## Scripts
 
-#### 1-getId.php
+#### I. 1-getId.php
 Adds Id, InternalId and Start Time (in UNIXTIME) to table `new`. Needs to be executed during the session.
 
 ###### Runs 4x per hour
-
-#### 2-getInfo.php
+#### II. 2-getInfo.php
 Makes an API request for the Session Report of every entry on table `new`. If the request is successfull transfers the data in `new` to the table `tutorings`.
 
 ###### Runs every hour
-
+#### IV. reports/4-downloadReports.php
+Downloads all reports requested by 2-getInfo.php, and updates the corresponding entry in the `reports` table.
+`Downloaded` column:
+- 0: New session
+- 1: Downloaded
 
 ## PHP Pages
 
-#### 3 - callback.php
+#### III. callback.php
 Callback page for 2-getInfo.php request. Inserts the link for downloading the report in the `Reports` table.
---
+++ reports/4-downloadReport.php
@@ -0,0 +1,81 @@
+<?php    
+    require_once '../connectDB.php';
+
+    $today = date("Y-m-d H:i:sZ");
+    echo "\n========== " . $today . "==========\n";
+
+
+    $sql = "SELECT Id, URL FROM `Relatorios` WHERE `Baixado` = 0;";
+    
+    try
+    {
+        $resultado = $conecta->query($sql);
+        
+        if($resultado != null) 
+        {
+            foreach($resultado as $linha) 
+            {
+                // Get Session Id
+                $id = $linha['Id'];
+
+                // Initialize a file URL to the variable
+                $url = rawurldecode($linha['URL']);
+                
+                // Initialize the cURL session
+                $ch = curl_init($url);
+                
+                // Inintialize directory name where
+                // file will be save
+                $dir = './';
+                
+                // Use basename() function to return
+                // the base name of file 
+                $file_name = basename($url, ".json");
+                $tamanho = strpos($file_name, "?");
+                $file_name = substr($file_name, 0, $tamanho);
+
+                // Save file into file location
+                $save_file_loc = $dir . $file_name;
+                
+                // Open file 
+                $fp = fopen($save_file_loc, 'wb');
+                
+                // It set an option for a cURL transfer
+                curl_setopt($ch, CURLOPT_FILE, $fp);
+                curl_setopt($ch, CURLOPT_HEADER, 0);
+                
+                // Perform a cURL session
+                curl_exec($ch);
+                
+                // Closes a cURL session and frees all resources
+                curl_close($ch);
+
+                // Close file
+                fclose($fp);
+                
+                /*
+                // Marca o Relatório como baixado
+                $sql2 = "UPDATE `Relatorios` SET `Baixado` = 1 WHERE `Id` = '" . $id . "';";
+                try
+                {
+                    $resultado2 = $conecta->query($sql2);
+
+                     // aaaa-mm-dd hh:mm:ss (the MySQL DATETIME format)
+                    $today = date("Y-m-d H:i:s");
+                    echo $today . ": Operação realizada com sucesso!\n";
+                }
+                catch(PDOException $e)
+                {
+                    echo 'ERRO!';
+                    echo $e;
+                }
+                */
+            }
+        }
+    }
+    catch(PDOException $e)
+    {
+        echo 'ERRO!';
+        echo $e;
+    }  
+?>
