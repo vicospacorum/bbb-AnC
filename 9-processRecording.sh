259d8f095a5360f1a3c12206b83671d6f2d5cfe1
vicospacorum <fdomingos@usp.br>
2021-04-28 07:02:35 -0300

    reduces video size

--- 9-processRecording.sh
+++ 9-processRecording.sh
@@ -1,2 +1,24 @@
-#
-ls -alh
+# Reduz o tamanho do video
+#f4515f7a041dcb0f19b228601a8bb9ea75474af9-1619544281443
+for video in $(ls /home/girassol/scripts/videos/*.mp4); do
+    echo "Video: $video";
+    dir=${video:0:30}
+    arquivo=${video:30:-4};
+    echo "Pasta: $dir";
+    echo "Arquivo: $arquivo";
+    echo
+    echo
+    arquivotmp=${dir}proc/${arquivo}_tmp.mp4
+    arquivonovo=${dir}proc/${arquivo}.mp4
+
+    mv -v $video $arquivotmp
+    ffmpeg -i $arquivotmp -vcodec libx265 -crf 24 $arquivonovo
+    
+    # Envia para a nuvem
+    mv -v $arquivonovo /home/girassol/scripts/videos/proc/nuvem/${arquivo}.mp4
+    #rm $arquivotmp
+
+    # Gera Link de compartilhamento
+
+    # Atualiza od bancos de dados
+done;
