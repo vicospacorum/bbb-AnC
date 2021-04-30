50f7f071fb235dd00845bde3175f3cc1f96b27c4
vicospacorum <fdomingos@usp.br>
2021-04-30 14:11:53 -0300

    upload and share video

--- 10-send2cloud.js
+++ 10-send2cloud.js
@@ -1,79 +1,53 @@
 /* 
-Google Drive API:
-Demonstration to:
-1. upload 
-2. delete 
-3. create public URL of a file.
-
+Google Drive API
 required npm package: googleapis
 */
 const { google } = require('googleapis');
 const path = require('path');
 const fs = require('fs');
 
-const CLIENT_ID = 'YOUR CLIENT ID';
-const CLIENT_SECRET = 'YOUR CLIENT SECRET';
+// Credentials
+const CLIENT_ID = '681703308523-7us65oqi5383dcob134t7jj218rv8nr3.apps.googleusercontent.com';
+const CLIENT_SECRET = 'T73cGj9tQ3BoR6EqWppeCB1k';
 const REDIRECT_URI = 'https://developers.google.com/oauthplayground';
 
-const REFRESH_TOKEN = 'YOUR REFRESH TOKEN GENERATED FROM oauthplayground';
+const REFRESH_TOKEN = '1//04nPLN9IBVdMyCgYIARAAGAQSNwF-L9IrcPfpUpXSHyoSMOFO_yfJaShsEsNqdxW-yy0knh5vt4p_-zcL_VPMyJJbqpTIr8OlRh4';
 
 const oauth2Client = new google.auth.OAuth2(
-  CLIENT_ID,
-  CLIENT_SECRET,
-  REDIRECT_URI
+    CLIENT_ID,
+    CLIENT_SECRET,
+    REDIRECT_URI
 );
 
 oauth2Client.setCredentials({ refresh_token: REFRESH_TOKEN });
 
 const drive = google.drive({
-  version: 'v3',
-  auth: oauth2Client,
+    version: 'v3',
+    auth: oauth2Client,
 });
 
-/* 
-filepath which needs to be uploaded
-Note: Assumes example.jpg file is in root directory, 
-though this can be any filePath
-*/
-const filePath = path.join(__dirname, 'example.jpg');
-
 async function uploadFile() {
-  try {
-    const response = await drive.files.create({
-      requestBody: {
-        name: 'example.jpg', //This can be name of your choice
-        mimeType: 'image/jpg',
-      },
-      media: {
-        mimeType: 'image/jpg',
-        body: fs.createReadStream(filePath),
-      },
-    });
-
-    console.log(response.data);
-  } catch (error) {
-    console.log(error.message);
-  }
+    try 
+    {
+      const response = await drive.files.create({
+        requestBody: {
+          name: arquivo, //This can be name of your choice
+          mimeType: 'video/mp4',
+          parents: ['1B9soAMgtdoD3DJlgF8mevoY7ZU8tkM-R'] // folderId 
+        },
+        media: {
+            mimeType: 'video/mp4',
+            body: fs.createReadStream(filePath),
+        },
+      });
+      console.log(response.data);
+    } catch (error) {
+      console.log(error.message);
+    }
 }
-
-// uploadFile();
-
-async function deleteFile() {
-  try {
-    const response = await drive.files.delete({
-      fileId: 'YOUR FILE ID',
-    });
-    console.log(response.data, response.status);
-  } catch (error) {
-    console.log(error.message);
-  }
-}
-
-// deleteFile();
 
 async function generatePublicUrl() {
   try {
-    const fileId = 'YOUR FILE ID';
     await drive.permissions.create({
       fileId: fileId,
       requestBody: {
@@ -82,13 +56,9 @@ async function generatePublicUrl() {
       },
     });
 
-    /* 
-    webViewLink: View the file in browser
-    webContentLink: Direct download link 
-    */
     const result = await drive.files.get({
       fileId: fileId,
-      fields: 'webViewLink, webContentLink',
+      fields: 'webViewLink',
     });
     console.log(result.data);
   } catch (error) {
@@ -96,4 +66,17 @@ async function generatePublicUrl() {
   }
 }
 
-// generatePublicUrl();
+const filePath = process.argv[3];
+const arquivo = filePath.substr(30);
+
+if (process.argv[2] == "send")
+{
+  
+  console.log("Uploading File");
+  uploadFile();
+}
+else if (process.argv[2] == "getLink")
+{
+  console.log("Sharing File");
+  generatePublicUrl();
+}
